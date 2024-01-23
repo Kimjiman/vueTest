9f8a981b9a5e38db65573d3cfab3853d4c0a0926
DESKTOP-MB020AA\user <kjmmaster@naver.com>
2024-01-23 17:52:00 +0900

    [Well]

--- src/views/layout/Header.vue
+++ src/views/layout/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
     <h1>Header</h1>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+    <span class="link"><router-link to="/">Home</router-link></span>
+    <span class="link"><router-link to="/about">About</router-link></span>
   </div>
 </template>
 <style scoped></style>
