eae7bd4f411c5e4171d271544b41cfa709731d09
DESKTOP-MB020AA\user <kjmmaster@naver.com>
2024-01-23 16:56:56 +0900

    [Feature] First

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App) // main
+
+app.use(createPinia()) // store
+app.use(router) // router
+
+app.mount('#app') // app.mount
