070ad28caa0fe4ffaab6061748c18197c4e83ef2
DESKTOP-MB020AA\user <kjmmaster@naver.com>
2024-01-23 17:32:47 +0900

    [Feature] 레이아웃 정의

---
+++ src/views/layout/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="header">
+    <h1>Header</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+<style scoped></style>
