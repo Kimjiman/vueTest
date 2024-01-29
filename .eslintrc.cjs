895a1fe2aa33e66ff8fa43246ebd3f7b0d07649a
DESKTOP-MB020AA\user <kjmmaster@naver.com>
2024-01-29 11:20:47 +0900

    [Well] eslint 설정 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,21 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  'vue/multi-word-component-names': [
-    'error',
-    {
-      ignores: ['default']
-    }
-  ]
-}
+    root: true,
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier/skip-formatting'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    'vue/multi-word-component-names': [
+        'error',
+        {
+            ignores: ['default'],
+        },
+    ],
+    rules: {
+        'no-unused-vars': 'warn', // 쓰지 않는 변수 사용시
+        'vue/no-unused-vars': 'warn', // 쓰지 않는 변수 사용시
+        'vue/multi-word-component-names': 'off', // 컴포넌트명 단일로 사용가능(on시에는 케밥케이스로 컴포넌트를 작성해야함)
+    },
+};
--- src/views/layout/Header.vue
+++ src/views/layout/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="header">
-    <h1>Header</h1>
-    <span class="link"><router-link to="/">Home</router-link></span>
-    <span class="link"><router-link to="/about">About</router-link></span>
-  </div>
+    <div class="header">
+        <h1>Header</h1>
+        <span class="link"><router-link to="/">Home</router-link></span>
+        <span class="link"><router-link to="/about">About</router-link></span>
+    </div>
 </template>
 <style scoped></style>
