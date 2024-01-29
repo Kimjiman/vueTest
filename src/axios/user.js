a87b28f431784cf50f8810bb45e20e4564e5c529
DESKTOP-MB020AA\user <kjmmaster@naver.com>
2024-01-29 20:33:17 +0900

    [Well] axios 스프링시큐리티 컨텐트타입 변경

--- src/axios/user.js
+++ src/axios/user.js
@@ -1,9 +1,13 @@
 import { axios } from './index';
 
 export const login = async (loginId, password) => {
-    const res = await axios.post('/loginProc', {
-        loginId: loginId,
-        password: password,
-    });
+    const res = await axios.post(
+        '/loginProc',
+        {
+            loginId: loginId,
+            password: password,
+        },
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
+    );
     return res;
 };
--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>Login</h1>
-    <div><input type="text" v-model="data.userId" placeholder="아이디" /></div>
+    <div><input type="text" v-model="data.loginId" placeholder="아이디" /></div>
     <div><input type="password" v-model="data.password" placeholder="패스워드" /></div>
     <button @click="loginAction()">로그인</button>
 </template>
@@ -10,12 +10,12 @@ import { ref } from 'vue';
 import { login } from '@/axios/user';
 
 const data = ref({
-    userId: null,
+    loginId: null,
     password: null,
 });
 
 const loginAction = async () => {
-    if (!data.value.userId || data.value.userId.trim() === '') {
+    if (!data.value.loginId || data.value.loginId.trim() === '') {
         return alert('아이디를 입력해주세요.');
     }
 
@@ -24,7 +24,7 @@ const loginAction = async () => {
     }
 
     try {
-        const res = await login(data.value.userId, data.value.password);
+        const res = await login(data.value.loginId, data.value.password);
         console.log(res);
     } catch (error) {
         console.error('loginAction Error:', error);
