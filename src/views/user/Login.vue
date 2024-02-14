42ad9249b2b9fe00526fc2470b3189912b052b6e
DESKTOP-MB020AA\user <kjmmaster@naver.com>
2024-02-14 18:00:29 +0900

    [Fix]

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,8 +1,8 @@
 <template>
     <h1>Login</h1>
-    <div><input type="text" v-model="data.loginId" placeholder="아이디" /></div>
-    <div><input type="password" v-model="data.password" placeholder="패스워드" /></div>
-    <el-button @click="login()">로그인</el-button>
+    <div><el-input type="text" v-model="data.loginId" placeholder="아이디" /></div>
+    <div><el-input type="password" v-model="data.password" placeholder="패스워드" /></div>
+    <el-button @click="login()" type="primary">로그인</el-button>
 </template>
 
 <script setup>
@@ -10,7 +10,7 @@ import { reactive } from 'vue';
 import userApi from '@/axios/user';
 import { useGlobalStore } from '@/store';
 import stringUtils from '@/utils/stringUtils';
-import { ElButton } from 'element-plus';
+import { ElInput, ElButton, ElMessage } from 'element-plus';
 
 const store = useGlobalStore();
 
@@ -21,11 +21,11 @@ const data = reactive({
 
 const login = async () => {
     if (stringUtils.isEmpty(data.loginId)) {
-        return alert('아이디를 입력해주세요.');
+        return ElMessage({ message: '아이디를 입력해주세요.', type: 'error' });
     }
 
     if (stringUtils.isEmpty(data.password)) {
-        return alert('비밀번호를 입력해주세요.');
+        return ElMessage({ message: '비밀번호를 입력해주세요.', type: 'error' });
     }
 
     try {
